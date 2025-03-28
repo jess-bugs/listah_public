c2938dbb4327ea2b97de85521640e9e7e36fe81d
bugs <gulf121909@gmail.com>
2025-03-28 10:26:30 +0000

    save progress - working on create account

---
+++ create-account/js/angular.js
@@ -0,0 +1,18 @@
+var app = angular.module('angularApp', []);
+
+app.controller('angular_controller', function($scope, $http, $timeout) {
+    
+    
+    $scope.unique_name_block = true;
+    $scope.user_details_block = false;
+    $scope.profile_pic_block  = false;
+    
+    
+    
+    $scope.form_submit = function() {
+                
+
+
+    }
+    
+});
